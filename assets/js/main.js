6312d092a9ee76baaae68641687fc173f7d06b06
Data-Science-MEng <106889359+K-Ahlschlager@users.noreply.github.com>
2024-01-28 21:56:49 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,3 +268,16 @@
   new PureCounter();
 
 })()
+
+function showImage() {
+	var fullImage = document.getElementById('fullImage');
+	var viewImageBtn = document.getElementById('viewImageBtn');
+
+	if (fullImage.style.display === 'none') {
+		fullImage.style.display = 'block';
+		viewImageBtn.innerText = 'Close Image';
+	} else {
+		fullImage.style.display = 'none';
+		viewImageBtn.innerText = 'View Image';
+	}
+}
